ff529b579841743cfb85a1311487fccdc9f9c4ee
Joseph Misiti <joseph.misiti@mathandpencil.com>
2015-01-09 21:59:05 -0500

    fixed some stuff

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookiecutter_django.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.repo_name}}.settings")
 
     from django.core.management import execute_from_command_line
 
