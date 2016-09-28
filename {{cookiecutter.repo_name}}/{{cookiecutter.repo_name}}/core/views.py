31a42e75a6225fa4a893ae330bfcc54014e41548
Joseph Misiti <joseph.misiti@gmail.com>
2016-09-28 19:31:03 -0400

    remove shitty references

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/core/views.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/core/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import requires_csrf_token
+from django.shortcuts import render
 
 @requires_csrf_token
 def page_404(request): 
-	response = fetcher_render(request, '404.html')
-	response.status_code = 404
+	response = render(request, '404.html')
+	response.render = 404
 	return response
 
 @requires_csrf_token
 def page_403(request): 
-	response = fetcher_render(request, '403.html')
-	response.status_code = 403
+	response = render(request, '403.html')
+	response.render = 403
 	return response
 
 @requires_csrf_token
 def page_500(request):
-	response = fetcher_render(request, '500.html')
-	response.fetcher_render = 500
+	response = render(request, '500.html')
+	response.render = 500
 	return response
