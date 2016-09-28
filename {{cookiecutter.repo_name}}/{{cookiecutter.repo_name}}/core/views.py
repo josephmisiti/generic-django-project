737a64cc80ee12bd9cb63abcc4b4e01570efaaaf
Joseph Misiti <joseph.misiti@gmail.com>
2016-09-28 19:30:22 -0400

    removed issue with imports in core.views

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/core/views.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/core/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import requires_csrf_token
 
-from utils.view_utils import custom_render
-
 @requires_csrf_token
 def page_404(request): 
 	response = fetcher_render(request, '404.html')
@@ -21,8 +19,3 @@ def page_500(request):
 	response = fetcher_render(request, '500.html')
 	response.fetcher_render = 500
 	return response
-
-
-def index(request):
-	return custom_render(request,'static/index.html', {})
-
