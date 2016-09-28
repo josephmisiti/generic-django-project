da6468b9d9eeb8a4bfccf8d5e0bc9142eaff2143
Joseph Misiti <joseph.misiti@gmail.com>
2016-09-28 12:50:41 -0400

    more updates

--- {{cookiecutter.repo_name}}/client/src/App.js
+++ {{cookiecutter.repo_name}}/client/src/App.js
@@ -6,16 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
+dddd
 export default App;
--- {{cookiecutter.repo_name}}/client/src/index.js
+++ {{cookiecutter.repo_name}}/client/src/index.js
@@ -4,6 +4,5 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
   document.getElementById('root')
 );
