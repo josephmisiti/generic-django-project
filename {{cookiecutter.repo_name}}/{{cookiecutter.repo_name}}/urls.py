9e36d6be10aa55a7dcc9eccb2b18665ecb89ee74
Joseph Misiti <josephmisiti@users.noreply.github.com>
2016-10-16 14:19:33 -0400

    Update urls.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from {{cookiecutter.repo_name}}.core.views import index_view
+
 urlpatterns = patterns('',
+    url(r'^$', index_view, name='index'),
     url(r'', include('{{cookiecutter.repo_name}}.core.urls',  namespace='api')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+    )
